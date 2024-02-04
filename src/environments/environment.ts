167bb6c7e873f6fdba470b066e53c98b0740745a
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-02-04 01:16:45 -0300

    Alterando variáveis de ambiente da production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     'GOOGLE_MAPS_API_KEY':'AIzaSyA2J8XefDFWdILAPuQUwzqOAhLoMjYeQSw',
-    'ROTAFOOD_API':'http://127.0.0.1:8000',
+    'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app/',
     'ENVMODE':'PRODUCTION'
 };
+ 
