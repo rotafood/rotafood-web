c0f0d176d07c9701591dbc6c48ac25446531d38c
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-02-04 01:19:35 -0300

    Mudando uma variavel de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     'GOOGLE_MAPS_API_KEY':'AIzaSyA2J8XefDFWdILAPuQUwzqOAhLoMjYeQSw',
-    'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app/',
+    'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app',
     'ENVMODE':'PRODUCTION'
 };
  
