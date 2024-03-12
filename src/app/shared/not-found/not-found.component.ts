a06a4af415bd65e92e647ad9e83ad22cac29b8de
Vinicius Gandolfi <81584546+ViniciusCostaGandolfi@users.noreply.github.com>
2024-03-12 16:52:20 -0300

    Altera ordem de importação

--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DefaultLayoutComponent } from '../../layouts/default-layout/default-layout.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { DefaultLayoutComponent } from '../default-layout/default-layout.component';
 
 @Component({
   selector: 'app-not-found',
