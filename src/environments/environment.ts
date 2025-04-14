34f041eee56bef60752d6650e4d2c3abbdc30faf
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2025-04-14 18:30:46 -0300

    Atualiza variavel de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     'GOOGLE_MAPS_API_KEY':'',
     'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app',
     'STRIPE_PRODUCT_LINK': 'https://buy.stripe.com/test_6oEfZ74nwfmT5uE7ss',
-    'PRINTER_EXTENSION_LINK': 'http://localhost:9000/rotafood/printer/extensao_impressora_v1.exe'
+    'PRINTER_EXTENSION_LINK': 'https://bucket-production-ec49.up.railway.app/rotafood/printer/extensao_impressora_v1.exe'
 };
