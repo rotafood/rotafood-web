7d6fff9cfea9524a092bfec8511bd96772b55b37
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2025-05-08 17:32:35 -0300

    Corrige link da extenssão

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     'ROTAFOOD_API':'https://rotafood-api-production.up.railway.app',
     'STRIPE_PRODUCT_LINK': 'https://buy.stripe.com/test_6oEfZ74nwfmT5uE7ss',
-    'PRINTER_EXTENSION_LINK': 'https://rotafood-bucket.up.railway.app/rotafood/printer/RotaFood_Printer-1.0.0.msi',
+    'PRINTER_EXTENSION_LINK': 'https://rotafood-bucket.up.railway.app/rotafood/printer/RotaFood_Printer-1.0.0.exe',
 };
